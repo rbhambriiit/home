28888cbcf1ca5608f6f21976003f887b7a0e0b2c
Rahul Bhambri <rbhambriiit@gmail.com>
2018-07-28 23:50:31 +0530

    Create train_data_to_json.py
    usage with code.
    
    this will create the output json file

---
+++ spacy_enhanced/train_data_to_json.py
@@ -0,0 +1,109 @@
+import spacy, json
+from spacy.gold import GoldCorpus, minibatch, biluo_tags_from_offsets, tags_to_entities
+
+
+def docs_from_offsets(nlp, gold):
+    """Create a sequence of Docs from a sequence of text, entity-offsets pairs."""
+    docs = []
+    for text, entities in gold:
+        doc = nlp(text)
+        entities = entities['entities']
+        tags = biluo_tags_from_offsets(doc, entities)
+        if entities:
+            for start, end, label in entities:
+                span = doc.char_span(start, end, label=label)
+                if span:
+                    doc.ents = list(doc.ents) + [span]
+        if doc.ents:  # remove to return documents without entities too
+            docs.append((doc, tags))
+    return docs
+
+
+def docs_to_trees(docs):
+    """Create spaCy JSON training data from a sequence of Docs."""
+    doc_trees = []
+    for d, doc_tuple in enumerate(docs):
+        doc, tags = doc_tuple
+        try:
+            tags_to_entities(tags)
+        except AssertionError:
+            print('Dropping {}'.format(d))
+            continue
+        if not tags:
+            print('Dropping {}'.format(d))
+            continue
+        sentences = []
+        for s in doc.sents:
+            s_tokens = []
+            for t in s:
+                token_data = {
+                    'id': t.i,
+                    'orth': t.orth_,
+                    'tag': t.tag_,
+                    'head': t.head.i - t.i,
+                    'dep': t.dep_,
+                    'ner': tags[t.i],
+                }
+                s_tokens.append(token_data)
+            sentences.append({'tokens': s_tokens})
+        doc_trees.append({
+            'id': d,
+            'paragraphs': [
+                {
+                    'raw': doc.text,
+                    'sentences': sentences,
+                }
+            ]
+        })
+    return doc_trees
+
+
+def train_data_to_json(train_data, nlp, output_json='train.json'):
+    """
+    Create spaCy JSON training data from a sequence as list available in
+    train_data format - standard to spaCy users!
+
+    modified and motivated
+    from https://support.prodi.gy/t/prodigy-annotations-to-spacy-train/243/11
+    https://github.com/explosion/spaCy/issues/2515
+
+    phase1 of prodigy_to_spacy : not required here.
+    train data is already in that format and no need of prodigy database
+
+    docs_from_offsets: used as-is.
+    docs_to_trees: used as-is.
+
+    save to output_json
+
+    """
+
+    offsets = train_data
+    # print 'offsets:', offsets
+
+    docs = docs_from_offsets(nlp, offsets)
+    # print 'docs:', docs
+
+    trees = docs_to_trees(docs)
+    # print 'trees:', trees
+
+    with open(output_json, 'wt') as f:
+        json.dump(trees, f)
+    print 'successfully converted to json:',  output_json
+
+    return True
+
+
+nlp = spacy.load('en')
+
+train_data = [
+(u"Uber blew through $1 million a week", {'entities':[(0, 4, 'ORG')]}),
+(u"Android Pay expands to Canada", {'entities':[(0, 11, 'PRODUCT'), (23, 30, 'GPE')]}),
+(u"Spotify steps up Asia expansion", {'entities':[(0, 8, "ORG"), (17, 21, "LOC")]}),
+(u"Google Maps launches location sharing", {'entities':[(0, 11, "PRODUCT")]}),
+(u"Google rebrands its business apps", {'entities':[(0, 6, "ORG")]}),
+(u"look what i found on google! joy", {'entities':[(21, 27, "PRODUCT")]})
+]
+
+
+train_data_to_json(train_data, nlp)
+
